ffdbc63be823235caa7fbffcc5473bfa6ac7f0a9
aquashdw <aquashdw@gmail.com>
2025-11-20 21:36:05 +0900

    system prompt + context

--- gemma.py
+++ gemma.py
@@ -28,32 +28,53 @@ print(output)
 
 # llama_cpp
 import os
-from llama_cpp import Llama
+from llama_cpp import Llama, ChatCompletionRequestSystemMessage, ChatCompletionRequestUserMessage, \
+    ChatCompletionRequestMessageContentPartText, ChatCompletionRequestAssistantMessage
 
 llm = Llama.from_pretrained(
 	repo_id='google/gemma-3-4b-it-qat-q4_0-gguf',
 	filename='gemma-3-4b-it-q4_0.gguf',
     hf_token=os.getenv('HF_TOKEN'),
     n_gpu_layers=-1,
+    n_ctx=4096,
     verbose=False,
 )
 
-# output_blob = llm.create_chat_completion(
-#     messages = [
-#         {
-#             'role': 'user',
-#             'content': [
-#                 {
-#                     'type': 'text',
-#                     'text': 'Have you seen Matrix 2: Reloaded? What is the meaning of the conversation the Architect and Neo had at the near end of the movie? answer with 3 ~ 5 senctences.'
-#                 },
-#             ]
-#         }
-#     ],
-# )
 
-# from pprint import pprint
-# pprint(output_blob.get('choices')[0].get('message').get('content'))
+messages=[
+    ChatCompletionRequestSystemMessage(
+        role='system',
+        content="""
+        you are a general assistant. 
+        you are assisting a person who wants to know essentials, and details matter only when necessary.
+        So unless requested, keep your answers around 3 ~ 5 sentences, 
+        pointing out the most significant information.
+        start the chat session with a greeting and introduction of yourself.
+        """
+    ),
+    ChatCompletionRequestUserMessage(
+        role='user',
+        content='',
+    )
+]
+
+output_blob = llm.create_chat_completion(messages=messages, max_tokens=1024)
+content = output_blob.get('choices')[0].get('message').get('content')
+
+from pprint import pprint
+pprint(content)
+
+messages.append(ChatCompletionRequestAssistantMessage(
+    role='assistant',
+    content=content,
+))
+
+
+def flush_buffer(buffer):
+    joined = ''.join(buffer)
+    print(''.join(buffer))
+    buffer.clear()
+    return joined
 
 
 while True:
@@ -61,30 +82,43 @@ while True:
         prompt = input('prompt: ')
         if prompt.startswith('.exit'):
             break
-        output_stream = llm.create_chat_completion(
-            messages=[
+        message = ChatCompletionRequestUserMessage(
+            role='user',
+            content=[
+                ChatCompletionRequestMessageContentPartText(
+                    type='text',
+                    text=prompt,
+                )
+            ]
+        )
+        messages.append({
+            'role': 'user',
+            'content': [
                 {
-                    'role': 'user',
-                    'content': [
-                        {
-                            'type': 'text',
-                            'text': prompt,
-                        },
-                    ]
-                }
+                    'type': 'text',
+                    'text': prompt,
+                },
             ],
+        })
+        output_stream = llm.create_chat_completion(
+            messages=messages,
             stream=True,
             max_tokens=2048,
         )
-        output_words = []
+        full_output = []
+        buffer = []
         for block in output_stream:
             delta = block.get('choices')[0].get('delta')
-            output_words.append(delta.get('content')) if delta.get('content') else None
-            if sum(map(len, output_words)) > 90:
-                print(''.join(output_words))
-                output_words.clear()
-        if output_words:
-            print(''.join(output_words))
+            buffer.append(delta.get('content')) if delta.get('content') else None
+            if sum(map(len, buffer)) > 90:
+                full_output.append(flush_buffer(buffer))
+        if buffer:
+            full_output.append(flush_buffer(buffer))
+
+        messages.append(ChatCompletionRequestAssistantMessage(
+            role='assistant',
+            content=''.join(full_output),
+        ))
 
     except KeyboardInterrupt:
         break
