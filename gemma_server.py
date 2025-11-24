1fad6d435925dbdc647775387947a83224f950f9
aquashdw <aquashdw@gmail.com>
2025-11-24 13:42:04 +0900

    properly chat with sessions and messages

--- gemma_server.py
+++ gemma_server.py
@@ -2,10 +2,11 @@ from concurrent.futures import ThreadPoolExecutor
 from pprint import pprint
 from typing import Optional
 
+import uuid
 import grpc
 
 from gemma import load_llm
-from chatting_pb2 import CompletionRequest, Role, CompletionResponseChunk
+from chatting_pb2 import CompletionRequest, Role, CompletionResponseChunk, StartChatResponseChunk
 from chatting_pb2_grpc import ChatServiceServicer, add_ChatServiceServicer_to_server
 from msg_factory import ChatMessage, ChatMessageContent
 from utils import forget_messages
@@ -13,49 +14,73 @@ from utils import forget_messages
 
 class ChatService(ChatServiceServicer):
     def __init__(self, n_ctx: int, system: Optional[str], ):
+        """
+        :param n_ctx: context window size
+        :param system: system prompt for each sessions
+        """
         self.llm = load_llm(n_ctx)
-        self.messages = []
+        self.messages = {}
+        self.system = None
         if system:
-            self.messages.append(ChatMessage(
-                    'system',
-                    system
-                ))
+            self.system = ChatMessage('system', system)
+
         self.tokens_per_gen = n_ctx // 8
         self.memory_limit = n_ctx - self.tokens_per_gen * 2
 
+    def StartChat(self, request, context):
+        session_id = str(uuid.uuid4())
+        self.messages[session_id] = [self.system, ]
+        messages = self.messages[session_id]
+        idx = 0
+        for content in self.yield_content(
+                messages,
+                ChatMessage('user', '')
+        ):
+            idx += 1
+            yield StartChatResponseChunk(
+                id=session_id,
+                idx=idx,
+                role=Role.ASSISTANT,
+                content=content,
+            )
+
     def RequestChat(self, request: CompletionRequest, context):
+        if request.id not in self.messages:
+            raise grpc.RpcError('id not found')
         prompt = request.prompt
-        self.messages.append(ChatMessage(
-            'user',
-            content=[
-                ChatMessageContent('text', text=prompt,)
-            ]
-        ))
+        idx = 0
+        for content in self.yield_content(
+                self.messages.get(request.id),
+                ChatMessage('user', content=[
+                    ChatMessageContent(content_type='text', text=prompt)
+        ])):
+            idx += 1
+            yield CompletionResponseChunk(
+                idx=idx,
+                role=Role.ASSISTANT,
+                content=content,
+            )
+
+    def yield_content(self, messages, message):
+        messages.append(message)
         output_stream = self.llm.create_chat_completion(
-            messages=self.messages,
+            messages=messages,
             stream=True,
             max_tokens=self.tokens_per_gen,
         )
-        idx = 0
         full_output = []
         for block in output_stream:
             delta = block.get('choices')[0].get('delta')
             if delta.get('content'):
-                idx += 1
                 content = delta.get('content')
                 full_output.append(content)
-                yield CompletionResponseChunk(
-                    idx=idx,
-                    role=Role.ASSISTANT,
-                    content=content,
-                )
-            # to check future usage
+                yield content
+            # leave for debugging
             else:
                 print('found block not content')
                 pprint(block)
-
-        self.messages.append(ChatMessage('assistant', ''.join(full_output)))
-        forget_messages(self.llm.tokenizer(), self.memory_limit, self.messages,)
+        messages.append(ChatMessage('assistant', ''.join(full_output)))
+        forget_messages(self.llm.tokenizer(), self.memory_limit, messages)
 
 
 if __name__ == '__main__':
--- test_client.py
+++ test_client.py
@@ -1,17 +1,20 @@
 from typing import Iterable
 
 import grpc
-from chatting_pb2 import CompletionRequest, CompletionResponseChunk
+
+from chatting_pb2 import CompletionRequest, StartChatResponseChunk, StartChatRequest
 from chatting_pb2_grpc import ChatServiceStub
 from utils import flush_buffer
 
 if __name__ == '__main__':
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = ChatServiceStub(channel)
-        message = CompletionRequest(prompt='hello')
-        output_stream: Iterable[CompletionResponseChunk] = stub.RequestChat(message)
+        output_stream: Iterable[StartChatResponseChunk] = stub.StartChat(StartChatRequest())
         buffer = []
+        session_id = None
         for chunk in output_stream:
+            if not session_id:
+                session_id = chunk.id
             buffer.append(chunk.content)
             if sum(map(len, buffer)) > 90:
                 flush_buffer(buffer)
@@ -24,7 +27,7 @@ if __name__ == '__main__':
                 if prompt.startswith('.exit'):
                     break
 
-                output_stream = stub.RequestChat(CompletionRequest(prompt=prompt))
+                output_stream = stub.RequestChat(CompletionRequest(id=session_id, prompt=prompt))
 
                 buffer = []
                 for chunk in output_stream:
